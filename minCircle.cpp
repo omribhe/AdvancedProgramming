b4b3dd8413c0ff7c8bfeaa686a6be3e86a679381
Uri <urigreitser@gmail.com>
2021-11-18 19:58:59 +0200

    Min circle start

--- minCircle.cpp
+++ minCircle.cpp
@@ -1,6 +1,7 @@
 #include "minCircle.h"
 #include <math.h>
 #include <vector>
+#include "anomaly_detection_util.cpp"
 
 float DistanceBetweenTwoPoints(Point p1, Point p2) {
     float distance = sqrt(pow(p2.x-p1.x,2)+pow(p2.y-p1.y,2));
@@ -33,7 +34,15 @@ Circle CreateCircleFromTwoPoints (Point p1, Point p2) {
     return Circle(center,r);
 }
 Circle CreateCircleFromThreePoints (Point p1, Point p2,Point p3) {
-
+    Line l1 = CreateLineFromTwoPoints(p1,p2);
+    Line l2 = CreateLineFromTwoPoints(p2,p3);
+    Point midL1((p1.x+p2.x)/2,(p1.y+p2.y)/2);
+    Point midL2((p2.x+p3.x)/2,(p2.y+p3.y)/2);
+    Line L1P = CreateLineFromPointAndSlope(midL1,l1.a*-1);
+    Line L2P = CreateLineFromPointAndSlope(midL2,l2.a*-1);
+    Point center = IntersectionPointOfTwoLines(L1P,L2P);
+    float radius = DistanceBetweenTwoPoints(center,p1);
+    return Circle(center,radius);
 }
 
 
